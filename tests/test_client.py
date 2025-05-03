70aa286c0c76a9ddbcdb026b7ccc9d1c3a746132
Michael Samsom <mike.samsom@asica.ca>
2025-05-03 10:36:59 -0600

    added rpc test cases

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from dotenv import dotenv_values
 
 from entity_disambiguator_py.awscurl import get_credentials_botocore
@@ -8,6 +10,18 @@ config = dotenv_values("test.env")
 client = EntityDisambiguatorLambdaClient(
     lambda_url=config["URL"], region=config["REGION"]
 )
+
+
+def __read(p):
+    with open(p, "r") as f:
+        s = f.read()
+    return s
+
+
+def __run_rpc(p):
+    data = __read(p)
+    resp = client.rpc_call(data)
+    assert resp["status_code"] == 200, f"{resp}"
 
 
 def test_get_credentials():
@@ -21,3 +35,13 @@ def test_get_credentials():
 def test_say_hello():
     r = client.say_hello()
     assert r["status_code"] == 200
+
+
+def test_concept_rpc():
+    test_directory = Path(os.path.abspath(os.path.dirname(__file__))).joinpath(
+        "test_payloads"
+    )
+
+    __run_rpc(test_directory.joinpath("concept_post.json"))
+    # __run_rpc(test_directory.joinpath("alias_name_post.json"))
+    # __run_rpc(test_directory.joinpath("alias_id_post.json"))
